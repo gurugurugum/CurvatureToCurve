8c7912e44c2739eb5979b0e7c86270c46c8f5d03
gurugurugum <wrsqq331@gmail.com>
2017-01-17 01:16:12 +0900

    modified a bit.

--- CurvatureToCurve.py
+++ CurvatureToCurve.py
@@ -1,17 +1,21 @@
 import scipy.integrate as integrate
-from math import cos, sin
+from math import cos, sin, log, exp
 
 def curvature(x):
-    return 1
+    return exp(x)
+    # if x <= 0:
+    #     return 0
+    # else:
+    #     return x * log(x)
 
 def intCurvature(x):
-    return integrate.quad(curvature, 0, x)
+    return integrate.quad(curvature, 0, x)[0]
 
 def xCoord(t):
-    return integrate.quad(lambda x: cos(intCurvature(x)), 0, t)
+    return integrate.quad(lambda x: cos(intCurvature(x)), 0, t)[0]
 
 def yCoord(t):
-    return integrate.quad(lambda x: sin(intCurvature(x)), 0, t)
+    return integrate.quad(lambda x: sin(intCurvature(x)), 0, t)[0]
 
 def curve(t):
     return [xCoord(t), yCoord(t)]
